adb52d9bfa8b051ade706f0acaabba091a3795ef
Yusuf Khan <yusuf.jkhan1@gmail.com>
2021-08-27 17:43:10 -0700

    hooks testing

--- hooks/post_gen_project.bat
+++ hooks/post_gen_project.bat
@@ -1,4 +1,3 @@
 SETLOCAL
 SET PATH = ".."
-Powershell.exe -executionpolicy remotesigned cd ..
-Powershell.exe -executionpolicy remotesigned Get-Location
+Powershell.exe -executionpolicy remotesigned Start-Job { cd .. ; Get-Location}
